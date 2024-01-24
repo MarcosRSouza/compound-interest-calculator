022e004f00becdd9d4dd0f12a2e64002964253b4
MarcosRSouza <marcosmrodrigues.souza@gmail.com>
2024-01-24 17:36:24 -0300

    feat (api): add more complex test suites and adjust function output

--- api/index.test.js
+++ api/index.test.js
@@ -9,7 +9,7 @@ describe('tests compound rate calculator variations', () => {
                 period: 1,
                 rate: 0.01,
                 initialCapital: 100,
-            })).toBe(101);
+            })).toBe("101.00");
         expect(
             index({
                 rateType: 'MONTH',
@@ -17,7 +17,7 @@ describe('tests compound rate calculator variations', () => {
                 period: 2,
                 rate: 0.01,
                 initialCapital: 100,
-            })).toBe(102.01);
+            })).toBe("102.01");
     });
     test('should calculate with aport', () => {
         expect(index({
@@ -27,7 +27,23 @@ describe('tests compound rate calculator variations', () => {
             rate: 0.01,
             initialCapital: 100,
             aport: 100,
-        })).toBe(303.01);
+        })).toBe("303.01");
+        expect(index({
+            rateType: 'MONTH',
+            periodType: 'MONTH',
+            period: 3,
+            rate: 0.01,
+            initialCapital: 500,
+            aport: 1000,
+        })).toBe("3545.25");
+        expect(index({
+            rateType: 'MONTH',
+            periodType: 'MONTH',
+            period: 5,
+            rate: 0.1,
+            initialCapital: 5000,
+            aport: 10000,
+        })).toBe("69103.55");
     });
     test.todo('period type is MONTH and rate type is YEAR');
     test.todo('period type is YEAR and rate type is MONTH');
