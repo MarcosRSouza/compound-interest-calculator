896902e62b6f5925af4f3864c80858cf96a3dd63
MarcosRSouza <marcosmrodrigues.souza@gmail.com>
2024-01-24 17:38:23 -0300

    feat (api): add calculator main feature

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
-const compundRateWithAport = ({ initialCapital, aport, period, periodType, rate, rateType }) => {
-    // let totalMonths = 180;
-    // let monthlyAport = 1000;
+const compundRateWithAport = ({ initialCapital, aport = 0, period, periodType, rate, rateType }) => {
     let totalAmmount = 0;
-    // let compoundRate = 0.1;
-    // M = C * (1+i)^t
-    // for (let i = 1; i <= totalMonths; i++) {
-    // if (i === 1) {
+    if (aport !== 0) {
+        for (let i = 1; i < period; i++) {
+            let interest = Math.pow(1 + rate, i);
+            totalAmmount += aport * interest;
+        }
+        totalAmmount += aport;
+    }
     let interest = Math.pow(1 + rate, period);
     totalAmmount += initialCapital * interest;
-    // }
-    // }
-    return totalAmmount;
+    return Number.parseFloat(totalAmmount).toFixed(2);
 }
 
 module.exports = compundRateWithAport;
