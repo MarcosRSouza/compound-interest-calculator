7b6f35f0a18ef4b457874dc5b51b897eaf10b8d7
MarcosRSouza <marcosmrodrigues.souza@gmail.com>
2024-03-14 16:12:39 -0300

    feat: initial form template

--- web/src/pages/Main.jsx
+++ web/src/pages/Main.jsx
@@ -2,10 +2,22 @@ import Input from '../components/Input';
 
 function Main() {
     return (
-        <>
-            <h1>Main Page</h1>
-            <Input />
-        </>
+        <form action="/hello-world" method="post">
+            <ul>
+                <li>
+                    <label htmlFor="initial-aport">Initial Aport</label>
+                    <input type="text" id="initial-aport" name="initial-aport" placeholder="$0,00" />
+                </li>
+                <li>
+                    <label htmlFor="monthly-aport">Monthly Aport</label>
+                    <input type="text" id="monthly-aport" name="monthly-aport" placeholder="$0,00" />
+                </li>
+                <li>
+                    <label htmlFor="interest">Interest</label>
+                    <input type="text" id="interest" name="interest" placeholder="0,00%" />
+                </li>
+            </ul>
+        </form>
     );
 }
 
